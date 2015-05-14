d1a98d775b14d22007af7207291ddc36c8a0895a
doom369 <doom369@gmail.com>
2015-05-14 18:53:17 +0300

    Boldy moved to file.

--- src/main/java/com/azakordonets/web/controller/ResetPasswordController.java
+++ src/main/java/com/azakordonets/web/controller/ResetPasswordController.java
@@ -3,12 +3,15 @@ package com.azakordonets.web.controller;
 import com.azakordonets.mail.MailSender;
 import com.azakordonets.web.entities.TokensPool;
 import com.azakordonets.web.entities.User;
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 /**
  * Created by Andrew Zakordonets
@@ -22,19 +25,24 @@ public class ResetPasswordController {
     private final String url;
     private final int port;
     private final TokensPool tokensPool;
+    private final String body;
 
     public ResetPasswordController(String url, int port, TokensPool tokensPool) throws IOException {
         this.mailSender = new MailSender();
         this.url = url;
         this.port = port;
+
+        URL bodyUrl = Resources.getResource("messageBody.txt");
+        this.body = Resources.toString(bodyUrl, Charsets.UTF_8);
         this.tokensPool = tokensPool;
+
     }
 
     public void sendResetPasswordEmail(String email, String token) {
         User user = new User(email);
         tokensPool.addToken(token, user);
         final String resetUrl = String.format("%s:%d/landing?token=%s&email=%s", url, port, token, email);
-        final String message = String.format("You have requested password reset. Please folow this link %s", resetUrl);
+        final String message = String.format(body, resetUrl);
         log.info("Sending token to {} address", email);
         mailSender.produceSendMailTask(email, "Password reset request", message).run();
     }
